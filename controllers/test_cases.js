559b1035108fce0baeedb41c021c81a9ea47e1f6
Tomás Carvalho <tomcarvalhoc12@gmail.com>
2018-12-04 20:11:59 +0000

    Add new fields to TestCase creation and update

--- controllers/test_cases.js
+++ controllers/test_cases.js
@@ -10,13 +10,22 @@ module.exports = {
             expected_payload_type: req.body.expected_payload_type,
             expected_payload_value: req.body.expected_payload_value,
             active: req.body.active,
+            attachgasfeejs: req.body.attachgasfeejs,
+            attachneojs: req.body.attachneojs,
+            attachgasjs: req.body.attachgasjs,
+            wallet_invokejs: req.body.wallet_invokejs,
+            invokehashjs: req.body.invokehashjs,
+            invokeparamsjs: req.body.invokeparamsjs,
         })
         .then(test_case => res.status(201).send(test_case))
-        .catch(error => res.status(400).send(JSON.parse(error)));
+        .catch(error => res.status(400).send(error));
     },
     list(req, res) {
         return TestCase
-        .findAll({where: { userId: req.user.id }})
+        .findAll({where: {
+            userId: req.user.id,
+            testSuiteId: null
+        }})
         .then(testCases=> res.status(200).send(testCases))
         .catch(error => res.status(400).send(error));
     },
@@ -73,6 +82,17 @@ module.exports = {
                 description: req.body.description || test_case.description,
                 contract_hash: req.body.contract_hash || test_case.contract_hash,
                 userId: req.user.id || test_case.userId,
+                transaction_hash: req.body.transaction_hash || test_case.transaction_hash,
+                event_type: req.body.event_type || test_case.event_type,
+                expected_payload_type: req.body.expected_payload_type || test_case.expected_payload_type,
+                expected_payload_value: req.body.expected_payload_value || test_case.expected_payload_value,
+                active: req.body.active || test_case.active,
+                attachgasfeejs: req.body.attachgasfeejs || test_case.attachgasfeejs,
+                attachneojs: req.body.attachneojs || test_case.attachneojs,
+                attachgasjs: req.body.attachgasjs || test_case.attachgasjs,
+                wallet_invokejs: req.body.wallet_invokejs || test_case.wallet_invokejs,
+                invokehashjs: req.body.invokehashjs || test_case.invokehashjs,
+                invokeparamsjs: req.body.invokeparamsjs || test_case.invokeparamsjs,
             })
             .then(() => res.status(200).send(test_case))  // Send back the updated test_case.
             .catch((error) => res.status(400).send(error));
