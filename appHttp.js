9b4ababd1c11cc5f784e6933c84c3388085190a5
Tomás Carvalho <tomcarvalhoc12@gmail.com>
2019-01-10 15:17:40 +0000

    Add consistent identing

--- appHttp.js
+++ appHttp.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
-const express  = require('express');
+const express = require('express');
 const http = require('http');
-const logger = require('morgan');             // log requests to the console (express4)
+const logger = require('morgan'); // log requests to the console (express4)
 const app = express();
-const bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+const bodyParser = require('body-parser'); // pull information from HTML POST (express4)
 const session = require('express-session');
-const passport = require('passport'), LocalStrategy = require('passport-local').Strategy,
+const passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
     BearerStrategy = require('passport-http-bearer');
 
-app.use(express.static(__dirname + '/'));                 // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/')); // set the static files location /public/img will be /img for users
 app.use(session({
     resave: false,
     saveUninitialized: true,
@@ -19,14 +20,16 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(logger('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({                                 // parse application/x-www-form-urlencoded
-    parameterLimit: 100000,                // bigger parameter sizes
-    limit: '5mb',                          // bigger parameter sizes
+app.use(logger('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({ // parse application/x-www-form-urlencoded
+    parameterLimit: 100000, // bigger parameter sizes
+    limit: '5mb', // bigger parameter sizes
     extended: false
 }));
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({
+    type: 'application/vnd.api+json'
+})); // parse application/vnd.api+json as json
 
 app.set('jwtTokenSecret', process.env.JWT_TOKEN_SECRET || 'SETSECRETINPRODUCTION');
 
@@ -44,13 +47,15 @@ const jwt = require('jwt-simple');
 
 // This is used for auth. http://www.passportjs.org/docs/
 passport.use(new LocalStrategy(
-    function(email, password, done) {
+    function (email, password, done) {
         userController.getUserByEmail(email).then(function (user) {
             if (!user) {
-                return done(null, false, { message: 'Invalid email or password.' });
+                return done(null, false, {
+                    message: 'Invalid email or password.'
+                });
             }
-            bcrypt.compare(password, user.password, function(err, res) {
-                if(res) {
+            bcrypt.compare(password, user.password, function (err, res) {
+                if (res) {
                     let expires = new Date();
                     expires.setDate((new Date()).getDate() + 5);
                     let token = jwt.encode({
@@ -63,7 +68,9 @@ passport.use(new LocalStrategy(
                         return done(null, user);
                     })
                 } else {
-                    return done(null, false, { message: 'Invalid email or password.' })
+                    return done(null, false, {
+                        message: 'Invalid email or password.'
+                    })
                 }
             });
         });
@@ -71,27 +78,29 @@ passport.use(new LocalStrategy(
 ));
 
 passport.use(new BearerStrategy(
-    function(token, done) {
+    function (token, done) {
         userController.getUserByToken(token).then(function (user) {
-          let decodedToken = jwt.decode(token, app.get('jwtTokenSecret'));
-          if (!user) {
-              return done(null, false);
-          }
-          if (new Date(decodedToken.expires) < new Date() || user.id != decodedToken.id) {
-              user.token = null;
-              return done(null, false);
-          }
-          return done(null, user, { scope: 'all' });
+            let decodedToken = jwt.decode(token, app.get('jwtTokenSecret'));
+            if (!user) {
+                return done(null, false);
+            }
+            if (new Date(decodedToken.expires) < new Date() || user.id != decodedToken.id) {
+                user.token = null;
+                return done(null, false);
+            }
+            return done(null, user, {
+                scope: 'all'
+            });
         });
     }
 ));
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
+passport.serializeUser(function (user, done) {
+    done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
-  done(null, user);
+passport.deserializeUser(function (user, done) {
+    done(null, user);
 });
 
 server.listen(8000 || process.env.PORT, (err) => {
@@ -113,7 +122,11 @@ app.post('/api/test_case',
 
 app.get('/api/test_cases/',
     function (req, res, next) {
-        if (!req.isAuthenticated()) {res.status(401).send({status: "Unauthorized"});}
+        if (!req.isAuthenticated()) {
+            res.status(401).send({
+                status: "Unauthorized"
+            });
+        }
         next();
     },
     passport.authenticate('bearer'),
@@ -124,7 +137,11 @@ app.get('/api/test_case/:testID', testController.retrieve);
 
 app.delete('/api/test_case/:testID',
     function (req, res, next) {
-        if (!req.isAuthenticated()) {res.status(401).send({status: "Unauthorized"});}
+        if (!req.isAuthenticated()) {
+            res.status(401).send({
+                status: "Unauthorized"
+            });
+        }
         next();
     },
     passport.authenticate('bearer'),
@@ -149,7 +166,11 @@ app.get('/api/test_suite/:testSuiteID', testSuiteController.retrieve);
 
 app.get('/api/test_suites/',
     function (req, res, next) {
-        if (!req.isAuthenticated()) {res.status(401).send({status: "Unauthorized"});}
+        if (!req.isAuthenticated()) {
+            res.status(401).send({
+                status: "Unauthorized"
+            });
+        }
         next();
     },
     passport.authenticate('bearer'),
@@ -158,7 +179,11 @@ app.get('/api/test_suites/',
 
 app.put('/api/test_suite/:testSuiteID',
     function (req, res, next) {
-        if (!req.isAuthenticated()) {res.status(401).send({status: "Unauthorized"});}
+        if (!req.isAuthenticated()) {
+            res.status(401).send({
+                status: "Unauthorized"
+            });
+        }
         next();
     },
     passport.authenticate('bearer'),
@@ -167,7 +192,11 @@ app.put('/api/test_suite/:testSuiteID',
 
 app.delete('/api/test_suite/:testSuiteID',
     function (req, res, next) {
-        if (!req.isAuthenticated()) {res.status(401).send({status: "Unauthorized"});}
+        if (!req.isAuthenticated()) {
+            res.status(401).send({
+                status: "Unauthorized"
+            });
+        }
         next();
     },
     passport.authenticate('bearer'),
@@ -177,14 +206,16 @@ app.delete('/api/test_suite/:testSuiteID',
 // ============================================================
 // ================== Users ===================================
 
-app.post('/api/user', 
-    function(req, res, next) {
+app.post('/api/user',
+    function (req, res, next) {
         if (req.body.password != req.body.confirmPassword) {
-            res.status(400).send({status: "Passwords don't match"});
+            res.status(400).send({
+                status: "Passwords don't match"
+            });
             return;
         }
 
-        bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
             req.body.password = hash;
             next()
         });
@@ -194,54 +225,73 @@ app.post('/api/user',
 
 app.get('/api/user/checkSessionToken',
     function (req, res, next) {
-        console.log(req.headers.authorization);
-        if (!req.isAuthenticated()) {res.status(401).send({status: "Unauthorized"}); return;}
+        if (!req.isAuthenticated()) {
+            res.status(401).send({
+                status: "Unauthorized"
+            });
+            return;
+        }
         next();
     },
     passport.authenticate('bearer'),
-    function (req, res){
-        res.status(200).send({status: "Ok", sessionID: req.sessionID });
+    function (req, res) {
+        res.status(200).send({
+            status: "Ok"
+        });
     }
 );
 
 app.post('/api/login',
     // Checks if user exists and if credentials match and generates a new session token
     passport.authenticate('local'),
-    function(req, res) {
+    function (req, res) {
         // If this function gets called, authentication was successful.
         // `req.user` contains the authenticated user.
         req.login(req.user, (err) => {
-            if (err) { res.status(500).send("Internal Server Error"); return;}
+            if (err) {
+                res.status(500).send("Internal Server Error");
+                return;
+            }
         });
-        res.status(200).send({ access_token: req.user.token});
-});
+        res.status(200).send({
+            access_token: req.user.token
+        });
+    });
 
 app.post('/api/logout/',
     function (req, res, next) {
-        if (!req.isAuthenticated()) {res.status(401).send({status: "Unauthorized"}); return;}
+        if (!req.isAuthenticated()) {
+            res.status(401).send({
+                status: "Unauthorized"
+            });
+            return;
+        }
         next();
     },
     passport.authenticate('bearer'),
-    function(req, res) {
+    function (req, res) {
         let user = req.user;
         user.token = null;
         user.save().then(() => {
-            req.session.destroy(function(err) {
-                if (err) { res.status(500).send("Internal Server Error") ; return;}
+            req.session.destroy(function (err) {
+                if (err) {
+                    res.status(500).send("Internal Server Error");
+                    return;
+                }
                 res.status(200).send("OK");
             })
         });
-});
+    });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
