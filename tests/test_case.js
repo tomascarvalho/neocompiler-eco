b47a7ff83b038c428d4cf84abe675df9e5d7878e
Tomás Carvalho <tomcarvalhoc12@gmail.com>
2019-01-14 21:53:38 +0000

    Add test case tests

--- tests/test_case.js
+++ tests/test_case.js
@@ -13,9 +13,15 @@ chai.use(chaiHttp);
 const app = require('../appHttp');
 
 const should = chai.should();
-let agent = chai.request.agent(app); // request agent used to mantain cookies between requests
+let agent = chai.request.agent(app); // request agent used to maintain cookies between requests
 let bearerToken = '';
 let expiredBearerToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiZXhwaXJlcyI6IjIwMTgtMDEtMTZUMTM6MzM6NTQuMjQ5WiJ9.NKoGPFJ2nN5p7WWYefXB_veVf1fMGxPvtsV-erZ-iPU';
+
+const user = {
+    email: "testuser@mail.com",
+    password: "uns4f3P4ss0rd",
+    confirmPassword: "uns4f3P4ss0rd"
+}
 
 
 describe('Test Cases', () => {
@@ -25,9 +31,22 @@ describe('Test Cases', () => {
             force: true
         }) // drops table and re-creates it
         .then(function () {
-            done(null);
+            chai.request(app)
+                .post('/api/user')
+                .send(user)
+                .end((err, res) => {
+                    done();
+                });
         }, function (err) {
             done(err);
+        })
+        .then(function() {
+            agent
+                .post('/api/login')
+                .send(user)
+                .end((err, res) => {
+                    bearerToken = res.body.access_token;
+                });
         });
     },
     after(function (done) {
@@ -162,12 +181,12 @@ describe('Test Cases', () => {
     });
 
     describe('/PUT test case', () => {
-        it('should be able to create a valid test case', (done) => {
-            const test_case = {
-                name: "Test Case Name",
+        it('should be able to edit an unsaved test case', (done) => {
+            const edited_test_case = {
+                name: "New Test Case Name",
                 description: "Test Case Description",
                 contract_hash: "3943e22ece58327eca17c790aa510e9cb211bb3d",
-                event_type: "SmartContract.Runtime.Log",
+                event_type: "SmartContract.Runtime.Notify",
                 expected_payload_type: "String",
                 expected_payload_value: "Contract was called",
                 attachgasfeejs: "0",
@@ -178,11 +197,157 @@ describe('Test Cases', () => {
                 invokeparamsjs: "[{\"type\":\"String\",\"value\":\"query\"},{\"type\":\"Array\",\"value\":[{\"type\":\"String\",\"value\":\"test.com\"}]}]"
             }
             chai.request(app)
-                .post('/api/test_case')
-                .send(test_case)
+                .put('/api/test_case/1')
+                .send(edited_test_case)
                 .end((err, res) => {
-                    res.should.have.status(201);
+                    res.should.have.status(200);
                     res.body.should.be.a('object');
+                    done();
+                });
+        });
+        // it('should not be able to edit an unowned test case with invalid contract_hash', (done) => {
+        //     const edited_test_case = {
+        //         name: "New Test Case Name",
+        //         description: "Test Case Description",
+        //         contract_hash: "3943e22ece4921isafaj7c790aa510e9cb211bb3d",
+        //         event_type: "SmartContract.Runtime.Notify",
+        //         expected_payload_type: "String",
+        //         expected_payload_value: "Contract was called",
+        //         attachgasfeejs: "0",
+        //         attachneojs: "0",
+        //         attachgasjs: "0",
+        //         wallet_invokejs: "wallet_0",
+        //         invokehashjs: "3943e22ece58327eca17c790aa510e9cb211bb3d",
+        //         invokeparamsjs: "[{\"type\":\"String\",\"value\":\"query\"},{\"type\":\"Array\",\"value\":[{\"type\":\"String\",\"value\":\"test.com\"}]}]"
+        //     }
+        //     chai.request(app)
+        //         .put('/api/test_case/1')
+        //         .send(edited_test_case)
+        //         .end((err, res) => {
+        //             res.should.have.status(422);
+        //             res.body.should.have.property('errors');
+        //             res.body.should.be.a('object');
+        //             res.body.should.have.property('errors');
+        //             res.body.errors.should.be.a('array');
+        //             res.body.errors[0].should.be.a('object');
+        //             res.body.errors[0].should.have.property('msg');
+        //             res.body.errors[0].should.have.property('param');
+        //             res.body.errors[0].should.have.property('param').eql('contract_hash');
+        //             res.body.errors[0].should.have.property('msg').eql('Invalid value');
+        //             done();
+        //         });
+        // });
+
+        // it('should not be able to edit an unowned test case with invalid event_type', (done) => {
+        //     const edited_test_case = {
+        //         name: "New Test Case Name",
+        //         description: "Test Case Description",
+        //         contract_hash: "3943e22ece4921isafaj7c790aa510e9cb211bb3d",
+        //         event_type: "DumbContrat.Runtime.Notify",
+        //         expected_payload_type: "String",
+        //         expected_payload_value: "Contract was called",
+        //         attachgasfeejs: "0",
+        //         attachneojs: "0",
+        //         attachgasjs: "0",
+        //         wallet_invokejs: "wallet_0",
+        //         invokehashjs: "3943e22ece58327eca17c790aa510e9cb211bb3d",
+        //         invokeparamsjs: "[{\"type\":\"String\",\"value\":\"query\"},{\"type\":\"Array\",\"value\":[{\"type\":\"String\",\"value\":\"test.com\"}]}]"
+        //     }
+        //     chai.request(app)
+        //         .put('/api/test_case/1')
+        //         .send(edited_test_case)
+        //         .end((err, res) => {
+        //             res.should.have.status(422);
+        //             res.body.should.have.property('errors');
+        //             res.body.should.be.a('object');
+        //             res.body.should.have.property('errors');
+        //             res.body.errors.should.be.a('array');
+        //             res.body.errors[0].should.be.a('object');
+        //             res.body.errors[0].should.have.property('msg');
+        //             res.body.errors[0].should.have.property('param');
+        //             res.body.errors[0].should.have.property('param').eql('contract_hash');
+        //             res.body.errors[0].should.have.property('msg').eql('Invalid value');
+        //             done();
+        //         });
+        // });
+
+        it('it should login an existent user', (done) => {
+            const user = {
+                username: "testuser@mail.com",
+                password: "uns4f3P4ss0rd",
+            }
+            agent
+                .post('/api/login')
+                .send(user)
+                .end((err, res) => {
+                    bearerToken = res.body.access_token;
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('access_token');
+                    res.body.access_token.should.be.a('string');
+                    done();
+                });
+        });
+
+        it ('should be able to own a test if authenticated', (done) => {
+            agent
+                .put('/api/test_case/1')
+                .set('Authorization', 'Bearer ' + bearerToken)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+
+        it('should be able to edit his saved test case', (done) => {
+            const edited_test_case = {
+                name: "A meaningful name!",
+                description: "Test Case Description",
+                contract_hash: "3943e22ece58327eca17c790aa510e9cb211bb3d",
+                event_type: "SmartContract.Runtime.Notify",
+                expected_payload_type: "String",
+                expected_payload_value: "Contract was called",
+                attachgasfeejs: "0",
+                attachneojs: "0",
+                attachgasjs: "0",
+                wallet_invokejs: "wallet_0",
+                invokehashjs: "3943e22ece58327eca17c790aa510e9cb211bb3d",
+                invokeparamsjs: "[{\"type\":\"String\",\"value\":\"query\"},{\"type\":\"Array\",\"value\":[{\"type\":\"String\",\"value\":\"test.com\"}]}]"
+            }
+            agent
+                .put('/api/test_case/1')
+                .set('Authorization', 'Bearer ' + bearerToken)
+                .send(edited_test_case)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('userId');
+                    res.body.should.have.property('userId').eql(1);
+                    done();
+                });
+        });
+
+        it('should not be able to edit other\'s saved test case', (done) => {
+            const edited_test_case = {
+                name: "I dont care about names!",
+                description: "Test Case Description",
+                contract_hash: "3943e22ece58327eca17c790aa510e9cb211bb3d",
+                event_type: "SmartContract.Runtime.Notify",
+                expected_payload_type: "String",
+                expected_payload_value: "Contract was called",
+                attachgasfeejs: "0",
+                attachneojs: "0",
+                attachgasjs: "0",
+                wallet_invokejs: "wallet_0",
+                invokehashjs: "3943e22ece58327eca17c790aa510e9cb211bb3d",
+                invokeparamsjs: "[{\"type\":\"String\",\"value\":\"query\"},{\"type\":\"Array\",\"value\":[{\"type\":\"String\",\"value\":\"test.com\"}]}]"
+            }
+            chai.request(app)
+                .put('/api/test_case/1')
+                .send(edited_test_case)
+                .end((err, res) => {
+                    res.should.have.status(403);
                     done();
                 });
         });
@@ -213,23 +378,5 @@ describe('Test Cases', () => {
         });
     });
 
-    describe('/POST login', () => {
-        it('it should login an existent user', (done) => {
-            const user = {
-                username: "testuser@mail.com",
-                password: "uns4f3P4ss0rd",
-            }
-            agent
-                .post('/api/login')
-                .send(user)
-                .end((err, res) => {
-                    bearerToken = res.body.access_token;
-                    res.should.have.status(200);
-                    res.body.should.be.a('object');
-                    res.body.should.have.property('access_token');
-                    res.body.access_token.should.be.a('string');
-                    done();
-                }); 
-        });
-    });
+    
 });
